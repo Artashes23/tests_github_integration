e77eb491c33319d4799b94296e392da06fcdb142
Artashes <100474936+Artashes23@users.noreply.github.com>
2023-01-31 22:14:21 +0400

    Update test_ideation.py

--- tests/test_ideation.py
+++ tests/test_ideation.py
@@ -1,9 +1,7 @@
 from .pages.create_ideation import Check_Ideations
 from dotenv import load_dotenv
 import os
-import sys
 
-sys.path.append("D:\a\tests_github_integration\tests_github_integration\tests\test_ideation.py")
 
 
 class Test_Ideations(Check_Ideations):
