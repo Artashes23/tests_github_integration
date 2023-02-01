c4e5305eca49bcaf0132d2c9367c6f3ec4193d98
Artashes <100474936+Artashes23@users.noreply.github.com>
2023-02-01 16:47:02 +0400

    Update test_settings.py

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -2,7 +2,7 @@ from ..pages.settings import check_settings_page
 import os
 
 class Test_Settings(check_settings_page):
-    load_dotenv
+    
     def test_goal_add_delete(self,setup):
         self.browser = setup
         self.browser.get(os.getenv('PROD_URL'))
