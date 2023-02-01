2437fb3b9898509a240f903bd48cc0e4a734aaf3
Artashes <100474936+Artashes23@users.noreply.github.com>
2023-02-01 18:18:23 +0400

    Update create_delete_initiative.py

--- pages/create_delete_initiative.py
+++ pages/create_delete_initiative.py
@@ -1,7 +1,7 @@
 from ..pages.base_page import BasePage
 from selenium.webdriver.common.keys import Keys
 from time import sleep
-from ..config.config import  Create_Init_Page,Main_Page_Data,Ideation_Data
+from ..config.config import  Create_Init_Page,Main_Page_Data,Ideation_Data,Settings_Page
 
 import os
 class Check_Create_Init(BasePage):
